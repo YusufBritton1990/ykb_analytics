20c47c698aab2d8d6cf252c64528f50abe5bae4f
Yusuf Britton <yusufbritton@yahoo.com>
2019-05-21 08:04:15 -0400

    displaying posts from models

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    #From Post model, gather posts that were publisted before now and order by
+    # its publisted date, descending order
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    # renders html if blog is requested from urls
+    return render(request, 'blog/post_list.html',{"posts":posts})
