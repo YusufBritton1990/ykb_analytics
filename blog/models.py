e274acc54af5b60525689ddd36206081c0c8a375
Yusuf Britton <yusufbritton@yahoo.com>
2019-05-21 07:32:57 -0400

    ability to make post from admin site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    """
+    author: Connection to User app. establish relationship to User
+        on_delete: if user is deleted, comments are deleted
+    title = title of post
+    text = text of post
+    created_date = date post is created
+    published_date = updated time of post, if edited
+    """
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        # If post is edited, will update time
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        # This will return the title 
+        return self.title
